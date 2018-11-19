ad23d8dd9d72879f19e3454724cadf70b59ff42f
Somrat <monnaf37@gmail.com>
2018-11-19 12:29:32 +0600

    modified clients slider and fixed responsive issues

--- static/js/main.js
+++ static/js/main.js
@@ -120,14 +120,30 @@ $(document).ready(function () {
         }
     });
 
-    $("#clients-logo").owlCarousel({
-
-        itemsCustom: false,
-        pagination: false,
-        items: 5,
-        autoplay: true,
-
-    });
+    $('.clients-logo-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
 
 
 });
