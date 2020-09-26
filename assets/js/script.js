c6c0cc993cea8012b325021e9e6542da2f916ac9
somratpro <abdulmonnafsomrat@gmail.com>
2020-09-26 10:07:47 +0600

    modified contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,56 +40,6 @@
         });
     };
 
-    /* ========================================================================= */
-    /*   Contact Form Validating
-    /* ========================================================================= */
-
-    $('#contact-form').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 4
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            subject: {
-                required: false,
-            },
-            message: {
-                required: true,
-            },
-        },
-        messages: {
-            user_name: {
-                required: "Come on, you have a name don't you?",
-                minlength: "Your name must consist of at least 2 characters"
-            },
-            email: {
-                required: "Please put your email address",
-            },
-            message: {
-                required: "Put some messages here?",
-                minlength: "Your name must consist of at least 2 characters"
-            },
-        },
-        submitHandler: function (form) {
-            $(form).ajaxSubmit({
-                type: "POST",
-                data: $(form).serialize(),
-                url: "sendmail.php",
-                success: function () {
-                    $('#contact-form #success').fadeIn();
-                },
-                error: function () {
-                    $('#contact-form #error').fadeIn();
-                }
-            });
-        }
-    });
-
-
 }(jQuery));
 
 
@@ -104,8 +54,6 @@ jQuery(document).ready(function () {
     }());
 
 });
-
-
 
 
 $(document).ready(function () {
